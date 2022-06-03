98db64ddf6a0627df66793ca88d7e7c026641a16
stonefulstm <stoneful.stm@gmail.com>
2022-06-03 19:17:05 -0300

    Exercício dia 6.1

--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -1,5 +1,22 @@
+function validaEntradas() {
+  const nome = document.getElementById('nome-completo').value.length;
+  const email = document.getElementById('e-mail').value.length;
+  const porque = document.getElementById('porque').value.length;
+  const resultado = nome >= 10 && nome <= 40 && email >= 10 && email <= 50 && porque <= 500; 
+  return resultado;
+}
+
 const botao = document.getElementById('enviar');
 botao.addEventListener('click', (event) => {
   event.preventDefault();
-  alert('Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip.');
+  if (!validaEntradas()) {
+    alert('Dados Inválidos');
+  } else {
+    alert('Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip.');
+  }
+});
+
+const usoImagem = document.getElementById('uso-imagem');
+usoImagem.addEventListener('change', () => {
+  botao.disabled = !usoImagem.checked;
 });
