3f26462dc64f0b4a2713c823dffc2cb911314a68
stonefulstm <stoneful.stm@gmail.com>
2022-06-22 15:26:35 -0300

    Exercicio 1

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/exercicio1.js
@@ -0,0 +1,15 @@
+const newEmployees = (employeeGenerator) => {
+  const employees = {
+    id1: employeeGenerator('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+    id2: employeeGenerator('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+    id3: employeeGenerator('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+  }
+  return employees;
+};
+
+const employeeGenerator = (fullName) => {
+  const email = fullName.toLowerCase().replace(/ /g, '_'); 
+  return {fullName, email: `${email}@betrybe.com`};
+};
+
+console.log(newEmployees(employeeGenerator));
